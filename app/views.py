29f9fff76081b0eb16b0083112d895a9b0efafb5
Joshua Beedle <josh.beedle@gmail.com>
2022-03-28 16:23:26 +0000

    brought in Taskmaster version 1

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
+import json
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+# from .models import User
+
+def index(request):
+    return render(request, 'app/index.html')
