c67d28030fe59ddadfd8ad5f4bb8e53fde5353c4
Joshua Beedle <josh.beedle@gmail.com>
2022-03-30 16:23:26 +0000

    corrected edit functionality, updated success alert

--- app/static/app/app.js
+++ app/static/app/app.js
@@ -9,11 +9,25 @@ const alertHolder = document.querySelector("#alertHolder");
 
 var tasks = [];
 
-function alert(message, type) {
+function alert(message) {
     const wrapper = document.createElement('div');
-    wrapper.innerHTML = `<div class="alert alert-${type} d-flex align-items-center" role="alert">
+    wrapper.innerHTML = `<div class="alert alert-danger d-flex align-items-center" role="alert">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
                                 <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                            </svg>
+                            <div class="ml-2">
+                                ${message}
+                            </div>
+                            <button type="button" class="btn-close ms-auto" data-bs-dismiss="alert" aria-label="Close"></button>
+                         </div>`;
+    alertHolder.append(wrapper);
+}
+
+function success(message) {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = `<div class="alert alert-success d-flex align-items-center" role="alert">
+                            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:">
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
                             </svg>
                             <div class="ml-2">
                                 ${message}
@@ -34,7 +48,7 @@ const taskFormHandler = function(event) {
 
     // Makes sure that Name and Type were filled out
     if (!taskNameInput || !taskTypeInput) {
-        alert("You need to fill out the task form!", 'danger');
+        alert("You need to fill out the task form!");
         return false;
     }
 
@@ -104,7 +118,7 @@ const createTaskEl = function(taskDataObj) {
 // Function to finish editing a task
 const completeEditTask = function(taskName, taskType, taskId) {
     // Find the matching task list item
-    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    const taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
 
     // Set new values
     taskSelected.querySelector("h3.task-name").textContent = taskName;
@@ -114,12 +128,13 @@ const completeEditTask = function(taskName, taskType, taskId) {
     for (var i = 0; i < tasks.length; i++) {
         if (tasks[i].id === parseInt(taskId)) {
             tasks[i].name = taskName;
-            tasks[i].type = tasktype;
+            tasks[i].type = taskType;
         }
     };
-    alert("Task Updated!");
 
-    completeEditTask();
+    success("Task updated successfully!");
+
+    saveTasks();
 
     // Resets the form
     formEl.removeAttribute("data-task-id");
