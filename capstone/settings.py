04857bd383ceaaaa474dfd639a3838d7f71c9f17
jbeedle19 <josh.beedle@gmail.com>
2022-03-30 19:10:54 -0400

    deploying to heroku

--- capstone/settings.py
+++ capstone/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import environ
+import django_heroku
 
 env = environ.Env(
     # set casting, default value
@@ -127,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
