6dfd0684ffd0f42cfe5d8182b3a730b71a60f522
jbeedle19 <josh.beedle@gmail.com>
2022-03-31 17:41:10 -0400

    changing DB to postgres

--- capstone/settings.py
+++ capstone/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import environ
-import django_heroku
+import dj_database_url
 
 env = environ.Env(
     # set casting, default value
@@ -85,10 +85,13 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 AUTH_USER_MODEL = "app.User"
 
