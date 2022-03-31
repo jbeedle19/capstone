6e7459bcc6e31cc63a37c7cc7227b31389efa73d
jbeedle19 <josh.beedle@gmail.com>
2022-03-31 17:55:52 -0400

    trying to correct DB sessions

--- capstone/settings.py
+++ capstone/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
